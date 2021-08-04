af0aa7eb941ea0752227299658a76c1721e54a5a
Felix Fernando <felixzhuang1@gmail.com>
2021-08-04 22:33:36 +0700

    463

---
+++ 463. Island Perimeter.py
@@ -0,0 +1,17 @@
+grid = [[1]]
+perimeter = 0
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        steps = [
+            [0, 1], [1, 0], [0, -1], [-1, 0]
+        ]
+        if grid[i][j] == 1:
+            count = 0
+            for step in steps:
+                if i + step[0] < 0 or i + step[0] >= len(grid) or j + step[1] < 0 or j + step[1] >= len(grid[0]):
+                    continue
+                elif grid[i + step[0]][j + step[1]] == 1:
+                    count += 1
+            perimeter += (4 - count)
+print(perimeter)
+                
