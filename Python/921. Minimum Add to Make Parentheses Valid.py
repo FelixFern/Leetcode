f5d644f2cb160442e777a8e1ed726169911a0041
Felix Fernando <felixzhuang1@gmail.com>
2024-10-09 13:16:01 +0700

    feat (py): added 921

---
+++ Python/921. Minimum Add to Make Parentheses Valid.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        if(len(s) == 0):
+            return True
+    
+        stack = []
+
+        for i in s:
+            if(len(stack) > 0):
+                if i == ")" and stack[len(stack) - 1] == "(":
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+
+        return len(stack) 
