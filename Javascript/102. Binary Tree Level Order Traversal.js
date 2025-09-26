e443a80e0a3301e1008730eb565a79781de5176b
felixfern <felixzhuang1@gmail.com>
2025-09-26 22:24:20 +0700

    feat (js): 102

---
+++ Javascript/102. Binary Tree Level Order Traversal.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  if (!root) return [];
+  const queue = [root];
+  const res = [];
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    let level = [];
+
+    for (let i = 0; i < levelSize; i++) {
+      const curr = queue.shift();
+      level.push(curr.val);
+      if (curr.left) queue.push(curr.left);
+      if (curr.right) queue.push(curr.right);
+    }
+    res.push(level);
+  }
+
+  return res;
+};
